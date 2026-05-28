7bc5c2e8b0554d07a9f4618a4635289fdbf11c96
phyue7646-creator <phyue7646@gmail.com>
2026-05-28 09:42:36 +0800

    Update streamlit_scale_app.py

--- streamlit_scale_app.py
+++ streamlit_scale_app.py
@@ -1,4 +1,4 @@
-````python
+
 import streamlit as st
 import google.generativeai as genai
 import json
@@ -690,4 +690,3 @@ elif st.session_state.page == "results":
             st.session_state.current_idea = 0
 
             st.rerun()
-````
