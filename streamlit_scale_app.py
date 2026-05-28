7f1c277859def020f76c5f899a53accc0374a4d7
phyue7646-creator <phyue7646@gmail.com>
2026-05-28 11:09:33 +0800

    Update streamlit_scale_app.py

--- streamlit_scale_app.py
+++ streamlit_scale_app.py
@@ -19,7 +19,7 @@ genai.configure(
     api_key=st.secrets["GOOGLE_API_KEY"]
 )
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 # =========================================================
 # LOAD SYSTEM PROMPT
